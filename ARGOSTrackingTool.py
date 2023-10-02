370fce5ef2cb7f56454e020466caa421854cff8b
yuezzzzzz <zhangyue@ad.unc.edu>
2023-10-02 12:17:58 -0400

    Task 3 read the data directly from the ARGOS file

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -25,3 +25,31 @@ obs_lon = lineData[7]     # Observation Longitude
   
 # Print information to the use
 print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
+
+#Create a variable pointing to the data file
+file_name = './data/raw/Sara.txt'
+
+#Create a file object from the file
+file_object = open(file_name,'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+lineString = line_list[200]
+
+#Split the string into a list of data items
+lineData = lineString.split()
+
+#Extract items in list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
+
+#Print the location of sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
