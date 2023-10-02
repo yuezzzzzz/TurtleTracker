526a15471d90bae1d59e389e1fa773fc62e2d518
yuezzzzzz <zhangyue@ad.unc.edu>
2023-10-01 21:30:15 -0400

    Initial commit of script document

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Yue Zhang (yz833@duke.edu)
+# Date:   Fall 2023
+#--------------------------------------------------------------
+
