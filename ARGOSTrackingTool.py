7ff79c6f4dd330f466224d5e4b9eb380d7c51c5d
yuezzzzzz <zhangyue@ad.unc.edu>
2023-10-08 15:11:49 -0400

    Task8: handling errors

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -237,4 +237,62 @@ for key in keys:
     lat = location[0]
     lon = location[1]
     print(f"On {user_date}, Sara the turtle was seen at {lat}d Lat, {lon}d Lgn")
+
+#Task8: Handling errors with if statement
+ #Ask user for a date
+user_date = input("Enter a date: ")
+
+#Create a variable pointing to the data file
+file_name = './data/raw/Sara.txt'
+
+#Create a file object from the file
+file_object = open(file_name,'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Initialize dictionaries
+date_dict = {}
+location_dict = {}
+
+#For loop
+for lineString in line_list:
+    #check if the line is a data line
+    if lineString[0] in ('#', 'u'):
+        continue
+    lineData = lineString.split()
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+
+    #Determine if location class criteria is met
+    if obs_lc in ("1", "2", "3"):
+        #Add items to dictionaries
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
     
+#Initialize key list
+keys = []
+
+#Loop through items in date_dict
+for item in date_dict.items():
+    key = item[0]
+    value = item[1]
+    if value == user_date:
+        keys.append(key)
+
+#Report if no record were found
+if len(keys) == 0:
+    print(f"No records were found on {user_date}")
+
+#Loop through keys and report locations
+for key in keys:
+    location = location_dict[key]
+    lat = location[0]
+    lon = location[1]
+    print(f"On {user_date}, Sara the turtle was seen at {lat}d Lat, {lon}d Lgn")
