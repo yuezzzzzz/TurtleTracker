5e52c2dc522818fee504d796343e02f4809811c8
yuezzzzzz <zhangyue@ad.unc.edu>
2023-10-08 14:50:02 -0400

    Task7: allow user to set date and show location

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -181,3 +181,60 @@ for lineString in line_list:
     
     #print the location of sara
     #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
+#Task7: Displaying information of a user-specified ARGOS observation
+#Task7.1: Asking the user to specify a date
+#Ask user for a date
+user_date = input("Enter a date: ")
+
+#Create a variable pointing to the data file
+file_name = './data/raw/Sara.txt'
+
+#Create a file object from the file
+file_object = open(file_name,'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Initialize dictionaries
+date_dict = {}
+location_dict = {}
+
+#For loop
+for lineString in line_list:
+    #check if the line is a data line
+    if lineString[0] in ('#', 'u'):
+        continue
+    lineData = lineString.split()
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+
+    #Determine if location class criteria is met
+    if obs_lc in ("1", "2", "3"):
+        #Add items to dictionaries
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
+    
+#Initialize key list
+keys = []
+
+#Loop through items in date_dict
+for item in date_dict.items():
+    key = item[0]
+    value = item[1]
+    if value == user_date:
+        keys.append(key)
+
+#Loop through keys and report locations
+for key in keys:
+    location = location_dict[key]
+    lat = location[0]
+    lon = location[1]
+    print(f"On {user_date}, Sara the turtle was seen at {lat}d Lat, {lon}d Lgn")
+    
